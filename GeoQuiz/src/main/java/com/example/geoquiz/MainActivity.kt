9ffbfe859cc28add37c9de9c96c60da399659d6e
wlsdn9297 <wlsdn9297@naver.com>
2022-08-21 14:22:25 +0900

    가로 방향 레이아웃 생성하기, 로깅하는 방법

--- GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.geoquiz.databinding.ActivityMainBinding
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var question: Question
     private lateinit var binding: ActivityMainBinding
 
     private val questionBank = listOf(
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -54,7 +59,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        Log.d(TAG, "Current question index: $currentIndex")
+
+        try {
+            question = questionBank[currentIndex]
+        } catch (ex: ArrayIndexOutOfBoundsException) {
+            Log.e(TAG, "Index was out of bounds", ex)
+            currentIndex = 5
+            question = questionBank[currentIndex]
+        }
+
+        val questionTextResId = question.textResId
         binding.questionTextView.setText(questionTextResId)
     }
 
@@ -68,5 +83,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
